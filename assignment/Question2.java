0d3c4093cfe7b87f43601b5ae91802495c7a5f5c
deepanshuTTN <deepanshu.mishra@tothenew.com>
2025-06-26 20:24:48 +0530

    question2

--- assignment/Question2.java
+++ assignment/Question2.java
@@ -6,11 +6,25 @@ interface MathOperation {
 }
 
 class MathUtil {
+    public int applyAdd(int a, int b){return a + b;}
+    public int applySubtract(int a, int b){return a - b;}
+
+    public static int applyMultiply(int a, int b){return a * b;}
 
 }
 
 public class Question2 {
     public static void main(String[] args) {
+        MathUtil uti = new MathUtil();
+
+        MathOperation addition = uti::applyAdd;
+        System.out.println(19 + " + " + 58 + " = " + addition.operate(19,58));
+
+        MathOperation subtraction = uti::applySubtract;
+        System.out.println(19 + " - " + 58 + " = " + subtraction.operate(19,58));
+
+        MathOperation multiplication = MathUtil::applyMultiply;
+        System.out.println(19 + " * " + 58 + " = " + multiplication.operate(19,58));
 
     }
 }
