cd6bb327f0bedf83ba5c8194b27987304f46da8b
deepanshuTTN <deepanshu.mishra@tothenew.com>
2025-06-26 17:01:28 +0530

    ques2

--- Scratch.java
+++ Scratch.java
@@ -1,10 +1,14 @@
-interface Animal{
-    void speak();
-}
+//interface Animal{
+//    void speak();
+//}
+
+import java.util.Objects;
+import java.util.function.Consumer;
 
 public class Scratch {
+    private int a;
     public static void main(String[] args) {
-        Animal cat = ()-> System.out.println("mew");
-        cat.speak();
+//        Animal cat = ()-> System.out.println("mew");
+//        cat.speak();
     }
 }
---
+++ assignment/Question2.java
@@ -0,0 +1,17 @@
+package assignment;
+
+@FunctionalInterface
+interface MathOperation {
+    int operate(int p, int q);
+}
+
+class MathUtil {
+
+}
+
+public class Question2 {
+    public static void main(String[] args) {
+
+    }
+}
+
