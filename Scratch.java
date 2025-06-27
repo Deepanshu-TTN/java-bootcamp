206c3290ceea6e1f2bb75b108598cf253c277c33
deepanshuTTN <deepanshu.mishra@tothenew.com>
2025-06-27 12:25:34 +0530

    some

--- Scratch.java
+++ Scratch.java
@@ -1,29 +1,50 @@
-/** <i>What would happen if a function is overloaded with 3 different classes and null is passed to it?</i><br>
- Compiler wont resolve as null can represent emptyness of all the objects<br><br>
-<i>How to fix?</i><br>
-Extend the overloaded passed classes and java now looks for ''the most specific method'' i.e. the 'most' child
-*/
-public class Scratch {
-    Scratch() {
-        System.out.print("Hi");
+import java.util.*;
+import java.util.function.Supplier;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+class Employee {
+    int id; String name; double netWorth;
+    Employee(int id, String name, double netWorth){
+        this.id = id;
+        this.name = name;
+        this.netWorth = netWorth;
     }
 
-    class A{}
-    class b extends A{}
-    class C extends b{}
+    void upWorth(double e){
+        this.netWorth *= e;
+    }
 
-    void display(A name) {
-        System.out.println(" " + name);
-    }
-    void display(b name) {
-        System.out.println(" " + name);
-    }
-    void display(C name) {
-        System.out.println(" " + name);
-    }
-    public static void main(String[] args) {
-        Scratch s = new Scratch();
-        s.display(null);
+    @Override
+    public String toString() {
+        return "Employee{" + "id=" + id +
+                ", name='" + name + '\'' +
+                ", netWorth=" + netWorth +
+                '}';
     }
 }
 
+public class Scratch {
+    private static Employee[] arrayOfEmps = {
+            new Employee(1, "Jeff Bezos", 100000.0),
+            new Employee(2, "Bill Gates", 200000.0),
+            new Employee(3, "Mark Zuckerberg", 300000.0)
+    };
+
+    public static void main(String[] args) {
+        List<Employee> employeeList = Arrays.asList(arrayOfEmps);
+        int[] ids = {1,2,3};
+
+        employeeList.stream().forEach(e -> e.upWorth(1.5));
+
+        System.out.println(Stream.of(Arrays.asList(ids)).count());
+        System.out.println(Stream.of(Arrays.asList(ids)).collect(Collectors.counting()));
+
+
+
+
+        Supplier<Stream> streamSupplier = ()->employeeList.stream().map(e->e.netWorth++).filter(e->e%2==0);
+        System.out.println(Arrays.asList(arrayOfEmps));
+        System.out.println(employeeList);
+    }
+}
